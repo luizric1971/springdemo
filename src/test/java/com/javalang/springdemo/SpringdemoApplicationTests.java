a3aa0ede185aa06d8e01a8ad3d3ee05a012b8f08
Luiz Ricardo <luizricardocordeiroalves@gmail.com>
2026-05-19 16:22:19 -0300

    Project created

---
+++ src/test/java/com/javalang/springdemo/SpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javalang.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
